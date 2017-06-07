7059160946d5ed0d98f40ab9c8b901d6c6c4679a
Andy Lushman <andylushman@gmail.com>
2017-06-07 13:01:39 -0600

    main.js file organized

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,1 +1,38 @@
+//=======================
+//GLOBAL VARIABLES
+//=======================
 
+var giphy = {
+  topics: [],
+  
+}
+
+//1. User enters text into input and that value is used to create a button
+//1.1 topic is added to an array of strings.
+//2.The button is appended to the #topics div
+//2.2 A loop that apends a button for each string in the array
+//3. When User clicks on button, the value is used to conduct a search trough the API
+//4. The results of the search are displayed in the #giphy-area
+//4.1 Under every gif, display its rating (PG, G, so on).
+//5.When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
+//5.1 When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+//6. A reset button should be added to clear the results
+
+
+
+
+
+
+//=======================
+//FUNCTIONS
+//=======================
+
+function topics(){
+
+
+};
+
+
+//=======================
+//MAIN PROCESS
+//=======================
