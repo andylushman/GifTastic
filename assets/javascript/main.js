45a6d6ae4bbade5409894ebe480bbcd1939566ac
Andy Lushman <andylushman@gmail.com>
2017-06-08 13:24:58 -0600

    Added comments to main.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -4,7 +4,7 @@
 
 var giphy = {
   searchValue: "",
-  countries: ["Canada", "United States of America", "Switzerland", "China"]
+  countries: ["Canada", "United States of America", "Switzerland", "China", ]
 }
 
 
@@ -17,34 +17,40 @@ var giphy = {
 function init() {
   for(i=0; i < giphy.countries.length; i++){
     console.log(giphy.countries[i]);
-    $("#giphy-area").append("<button class='btn-primary topic-btn'>" + giphy.countries[i] + "</button>")
+    $("#topics").append("<button class='btn-primary topic-btn'>" + giphy.countries[i] + "</button>")
   };
 };
 
 function search() {
+  //Empty the html giphy-area after each search
   $("#giphy-area").html("");
   giphy.searchValue = $("#search").val().trim();
+  //Set queryURL for AJAX Request
   var queryURL= "http://api.giphy.com/v1/gifs/search?q="+ giphy.searchValue +"&api_key=dc6zaTOxFJmzC&limit=10";
+
+  //AJAX Request
   $.ajax({
     url: queryURL,
     method: 'GET',
   }).done((response) => {
     console.log(response);
     for(i = 0; i < response.data.length; i++){
-      //console.log for debugging
-      console.log(response.data[i].rating);
       //Add raiting and img to html
       $("#giphy-area").append("<div class= 'gif-div'>Raiting: " +response.data[i].rating+ '<br>'+ "<img src='"+response.data[i].images.downsized_still.url+"'class= 'gif-img'></div>");
     }; $("#search").val("")
   });
   //create button function
   createTopicBtn();
-  init();
 };
 
 function createTopicBtn() {
   var searchText = $("#search").val().trim();
+  //Empty the countries array
+  giphy.countries= [];
+  //Add new country to the array
   giphy.countries.push(searchText);
+  //Run init()
+  init();
 };
 
 // function pressTopicBtn () {
@@ -68,6 +74,7 @@ function createTopicBtn() {
 //=======================
 //MAIN PROCESS
 //=======================
+//Initialize on start
 init();
 
 //When the Submit button is clicked the search function is called
